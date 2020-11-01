c364d6a678ad609ab2e3a231a291fcf060d217af
Dem19 <Danya_melniK@mail.ru>
2020-11-01 14:56:24 +0500

    first commit

--- homeworks/task3.py
+++ homeworks/task3.py
@@ -5,3 +5,23 @@
 В классе Position реализовать методы получения полного имени сотрудника (get_full_name) и дохода с учетом премии (get_total_income).
 Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные, проверить значения атрибутов, вызвать методы экземпляров).
 """
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+    def get_full_income(self):
+        return sum(self._income.values())
+
+class Position(Worker):
+    def get_full_name(self):
+        return self.name + ' ' + self.surname
+
+    def get_total_income(self):
+        return self.get_full_income()
+
+Jack = Position('Jack', 'Daniels', 'barman', 20000, 15000)
+print(Jack.get_full_name())
+print(Jack.get_total_income())
